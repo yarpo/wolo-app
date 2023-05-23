3bb37f84a8bb433182f989869142cf7a5e5cd8b1
Filiposki54 <filip.kierczynski@gmail.com>
2023-05-23 11:44:15 +0200

    WOLO-22 (#2)
    * Wolo-app setup
    
    I created a wolo-app skeleton:
    Added react project with required libraries
    Added Unit Tests (1=1)
    Added i18next and internationalizations:
    -Polish
    -English
    -Russian
    -Ukrainian
    
    * WOLO-22
    
    Moved translation to separate JSON's
    I have installed the appropriate dependencies "i18next-http-backend"
    Translations are now located at
    public/locales/{$language}/translation.js
    
    ---------
    
    Co-authored-by: Patryk Jar <patryk.jar@hmcts.net>

--- src/i18n.js
+++ src/i18n.js
@@ -1,33 +1,15 @@
-import i18next from 'i18next';
+import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 
-i18next
+i18n
 .use(initReactI18next)
 .use(LanguageDetector)
+.use(Backend)
 .init({
   debug: true,
   fallbackLng: 'en',
-  resources: {
-    en:{
-      translation:{
-        hello: 'Hello world'
-      }
-    },
-    pl:{
-      translation:{
-        hello: 'Witaj świecie'
-      }
-    },
-    uk:{
-      translation:{
-        hello: 'Привіт Світ'
-      }
-    },
-    ru:{
-      translation:{
-        hello: 'Привет, мир'
-      }
-    }
-  }
 });
+
+export default i18n;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-
+import './index.css';
 import './i18n.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
