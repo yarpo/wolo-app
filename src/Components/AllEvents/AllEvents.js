de753b3b0d213f284dece84ecfc4d83f42b542ba
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2023-06-03 10:10:14 +0200

    playing with css
    playing with css

--- src/Components/AllEvents/AllEvents.js
+++ src/Components/AllEvents/AllEvents.js
@@ -1,20 +1,83 @@
 import React, { useState } from 'react';
-import EventCard from '../EventCard/EventCard';
 import { VscChevronDown, VscChevronUp } from "react-icons/vsc";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import EventCard from '../EventCard/EventCard';
 import './AllEvents.css';
-
 
 const AllEvents = () => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const locations = ['Zaspa', 'Chełm', 'Wrzeszcz'];
+    const categories = ['Edukacja', 'Sport', 'Kultura', 'Pomoc'];
+    const organisations = ['1', '2', '3', '4'];
+    const ages = ['None', '18', '16'];
 
     const handleToggle = () => {
         setIsOpen(!isOpen);
     };
 
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
     return (
         <div id="container">
             <div id="filters">
-                {isOpen && <p>This is menu</p>}
+                {isOpen && 
+                (
+                    <>
+                        <div id="options">
+                            <DatePicker
+                                id="datePicker"
+                                selected={selectedDate}
+                                onChange={handleDateChange}
+                                dateFormat="dd/MM/yyyy"
+                                placeholderText="Select a date"
+                            />
+                            
+                            <select id="selectInput">
+                                <option value="" disabled selected>Location</option>
+                                {locations.map((location, index) => (
+                                <option key={index} value={location}>
+                                    {location}
+                                </option>
+                                ))}
+                            </select>
+
+                            <select id="selectInput">
+                                <option value="" disabled selected>Category</option>
+                                {categories.map((category, index) => (
+                                <option key={index} value={category}>
+                                    {category}
+                                </option>
+                                ))}
+                            </select>
+
+                            <select id="selectInput">
+                                <option value="" disabled selected>Organisations</option>
+                                {organisations.map((organisation, index) => (
+                                <option key={index} value={organisation}>
+                                    {organisation}
+                                </option>
+                                ))}
+                            </select>
+
+                            <select id="selectInput">
+                                <option value="" disabled selected>Age restrictions</option>
+                                {ages.map((age, index) => (
+                                <option key={index} value={age}>
+                                    {age}
+                                </option>
+                                ))}
+                            </select>
+                        </div>
+                    </>
+                )
+                }
+                <br />
                 <p id="toggle-filters" onClick={handleToggle}> 
                 {isOpen ? (
                     <>
