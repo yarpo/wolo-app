081b8ed0dcba7a99d09cdbbc6f33bd51d1d919a0
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2023-06-03 02:09:56 +0200

    Create toggle menu
    Create toggle menu

--- src/Components/AllEvents/AllEvents.js
+++ src/Components/AllEvents/AllEvents.js
@@ -1,10 +1,32 @@
-import './AllEvents.css';
+import React, { useState } from 'react';
 import EventCard from '../EventCard/EventCard';
+import { VscChevronDown, VscChevronUp } from "react-icons/vsc";
+import './AllEvents.css';
+
 
 const AllEvents = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div id="container">
-            
+            <div id="filters">
+                {isOpen && <p>This is menu</p>}
+                <p id="toggle-filters" onClick={handleToggle}> 
+                {isOpen ? (
+                    <>
+                        Schowaj Menu <VscChevronUp />
+                    </>
+                ) : (
+                    <>
+                        Rozwiń Menu <VscChevronDown />
+                    </>
+                )}
+                </p>
+            </div>
 
             <EventCard />
             <EventCard />
