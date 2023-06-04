d7522f23dbd0b7117ab7a4376cee89d305a42c24
Filiposki54 <filip.kierczynski@gmail.com>
2023-06-04 14:48:10 +0200

    More work done :`) (#14)

--- src/Components/EventCard/EventCard.js
+++ src/Components/EventCard/EventCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { VscLocation, VscBrowser, VscOrganization } from 'react-icons/vsc';
 import { BiTime } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 import logo from '../../images/event-img.jpg';
 import './EventCard.css';
 
@@ -9,34 +10,36 @@ const EventCard = ({ event }) => {
     const { t, i18n } = useTranslation();
 
     return (
-        <div className="card">
-            <div id="card-img">
-                <img src={logo} alt={event.title} />
+        <Link to="/details" style={{ textDecoration: 'none' }}>
+            <div className="card">
+                <div id="card-img">
+                    <img src={logo} alt={event.title} />
+                </div>
+                <div id="information">
+                    <h2>{event.title}</h2>
+                    <ul>
+                        <li>
+                            <VscLocation className="icon" /> <strong>{t('location')}:</strong> {event.location}
+                        </li>
+                        <li>
+                            <VscBrowser className="icon" /> <strong>{t('date')}:</strong> {event.date}
+                        </li>
+                        <li>
+                            <BiTime className="icon" /> <strong>{t('time')}:</strong> {event.time}
+                        </li>
+                        <li>
+                            <VscOrganization className="icon" /> <strong>{t('organizedBy')}:</strong> {event.organizedBy}
+                        </li>
+                        <li>
+                            <strong>{t('needed')}:</strong> {event.participantsNeeded}
+                        </li>
+                        <li>
+                            <strong>{t('signedIn')}:</strong> {event.participantsSignedIn}
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div id="information">
-                <h2>{event.title}</h2>
-                <ul>
-                    <li>
-                        <VscLocation className="icon" /> <strong>{t('location')}:</strong> {event.location}
-                    </li>
-                    <li>
-                        <VscBrowser className="icon" /> <strong>{t('date')}:</strong> {event.date}
-                    </li>
-                    <li>
-                        <BiTime className="icon" /> <strong>{t('time')}:</strong> {event.time}
-                    </li>
-                    <li>
-                        <VscOrganization className="icon" /> <strong>{t('organizedBy')}:</strong> {event.organizedBy}
-                    </li>
-                    <li>
-                        <strong>{t('needed')}:</strong> {event.participantsNeeded}
-                    </li>
-                    <li>
-                        <strong>{t('signedIn')}:</strong> {event.participantsSignedIn}
-                    </li>
-                </ul>
-            </div>
-        </div>
+        </Link>
     );
 };
 
