db1d02bf10b1af0c3a52400ef552d95e40a3d0b4
Filiposki54 <filip.kierczynski@gmail.com>
2023-06-04 13:19:18 +0200

    Revert "Wolo 26 home page (#12)"
    This reverts commit 8de24403423d43405c513771fb04548da236350e.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { useTranslation } from 'react-i18next';
 import { Routes, Route } from "react-router-dom"
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import Details from './Components/Details/Details';
-import Hero from './Components/Hero/Hero';
 import AllEvents from './Components/AllEvents/AllEvents';
 
 function App() {
@@ -12,7 +10,6 @@ function App() {
     <>
       <div className="App">
         <Navbar />
-        <Hero />
         <Routes>
           <Route path="/" />
           <Route path="/events" element={<AllEvents />} />
--- src/Components/Hero/Hero.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Hero from './Hero';
-
-describe('Hero', () => {
-  it('rendering without errors', () => {
-    render(<Hero />);
-  })
-});
