285e4f6da592192b59191d9a35c901d48010760f
Patryk Jar <patryk.jar@hmcts.net>
2023-06-14 13:12:10 +0200

    Mock translations in unit tests

--- src/Components/MoreEventsFromOrganizer/MoreEvents.test.js
+++ src/Components/MoreEventsFromOrganizer/MoreEvents.test.js
@@ -6,4 +6,13 @@ describe('MoreEvents', () => {
     it('rendering without errors', () => {
         render(<MoreEvents />);
     })
+
+     it('should render the text "More Events from this Organizer"', () => {
+        const { getByText } = render(<MoreEvents />);
+        // we use i18n to translate the text so it's not text, but key passed
+         // to translate function (mocked in setupTests.js)
+        const text = getByText('moreEventsFromThisOrganizer');
+        expect(text).toBeInTheDocument();
+     });
+
 });
