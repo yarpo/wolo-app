7a27bcd21097cf8bdd1b6866b7e7da8549cd0f1f
Patryk Jar <patryk.jar@hmcts.net>
2023-06-14 13:13:41 +0200

    reformatted code

--- src/Components/MoreEventsFromOrganizer/MoreEvents.test.js
+++ src/Components/MoreEventsFromOrganizer/MoreEvents.test.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import MoreEvents from './MoreEvents';
 
 describe('MoreEvents', () => {
-    it('rendering without errors', () => {
-        render(<MoreEvents />);
-    })
+  it('rendering without errors', () => {
+    render(<MoreEvents/>);
+  });
 
-     it('should render the text "More Events from this Organizer"', () => {
-        const { getByText } = render(<MoreEvents />);
-        // we use i18n to translate the text so it's not text, but key passed
-         // to translate function (mocked in setupTests.js)
-        const text = getByText('moreEventsFromThisOrganizer');
-        expect(text).toBeInTheDocument();
-     });
-
+  it('should render the text "More Events from this Organizer"', () => {
+    const {getByText} = render(<MoreEvents/>);
+    // we use i18n to translate the text so it's not text, but key passed
+    // to translate function (mocked in setupTests.js)
+    const text = getByText('moreEventsFromThisOrganizer');
+    expect(text).toBeInTheDocument();
+  });
 });
