8d359a26b05823c7dc074909f2482f48d9b82092
Filiposki54 <filip.kierczynski@gmail.com>
2023-07-14 15:55:52 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import AllEvents from './Components/AllEvents/AllEvents';
 import Details from './Components/Details/Details';
+import Hero from './Components/Hero/Hero';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Hero/>}/>
           <Route path="/events" element={<AllEvents />} />
           <Route path="/details" element={<Details />} />
         </Routes>
