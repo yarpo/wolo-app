899ccaaa611aeabaf68af65ff4dd1f4db343c5df
Filiposki54 <filip.kierczynski@gmail.com>
2023-11-29 18:34:07 +0100

    Wolo 112 login component (#37)
    * Seting up singup/login
    
    Seting up both components
    
    * Routing
    
    Added Routing
    
    * Login component structure
    
    Structure is done, it just need style now
    
    * Update Login.js
    
    * Update Login.js
    
    * Update Signup.js

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { Formik } from "formik";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="login-Div" style={{ textAlign: 'center', marginTop: '50px' }}>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <p>Continue your journey as a volunteer</p>
+            <button type="submit">G-Continue with Google</button>
+            <p>Or continue with your email address</p>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+            <br />
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+            <br />
+            <input
+              type="checkbox"
+              id="showPassword"
+              name="showPassword"
+            />
+            <label htmlFor="showPassword">Show my password</label>
+            <br />
+            <input
+              type="checkbox"
+              id="keepMeLoggedIn"
+              name="keepMeLoggedIn"
+            />
+            <label htmlFor="keepMeLoggedIn">Keep me logged in</label>
+            <br />
+            <button type="submit" disabled={isSubmitting}>
+              Log in
+            </button>
+            <p>
+              Dont have an account? <Link to="/signup">Register now</Link >
+            </p>
+          </form>
+        )}
+      </Formik>
+    </div>
+  )
+}
+
+export default Login; 
---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Formik } from "formik";
+
+const Signup = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <ha1>Signup</ha1>
+    </div>
+  )
+}
+
+export default Signup; 
