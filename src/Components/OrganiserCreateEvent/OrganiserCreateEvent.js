375d0becd8db2cedb4daa2ce2f83606c9bc2d84e
Kinga <99564630+KinWaj@users.noreply.github.com>
2023-12-04 20:09:13 +0100

    Wolo 108 create component for creating events by organiser (#42)
    * Make form for creating event by organiser
    
    made basic form for creating events
    
    * update basic form for creating events by organiser
    
    made some updates to layout of the form. Added Shifts section
    
    * Fix files based on review comments
    
    Deleted unnecessary new lines, removed one of <br /> and replaced div with span in shifts column

---
+++ src/Components/OrganiserCreateEvent/OrganiserCreateEvent.js
@@ -0,0 +1,187 @@
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import '../../styles/organiser-create-event.scss';
+
+const OrganiserCreateEvent = () => {
+
+  const initialValues = {
+    title: '',
+    description: '',
+    picture: '',
+    addressStreet: '',
+    addressNumber: '',
+    addressDescription: '',
+    district: '',
+    categories: '',
+    peselVerification: false,
+    volunteerAgreement: false,
+  };
+
+  const validate = values => {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Title is required';
+  }
+
+  if (!values.description) {
+    errors.description = 'Description is required';
+  }
+
+  if (!values.district) {
+    errors.district = 'District is required';
+  } 
+
+  if (!values.categories) {
+    errors.categories = 'At least one category is required';
+  }
+  
+  if (!values.addressStreet) {
+    errors.categories = 'At least one category is required';
+  }
+  
+  if (!values.addressNumber) {
+    errors.categories = 'At least one category is required';
+  }
+  
+  if (!values.addressDescription) {
+    errors.categories = 'At least one category is required';
+  }
+  
+  return errors;
+};
+
+  return (
+    <div className="organiser_create_event_div">
+      <h1 className="organiser_create_event_title">Create Event</h1>
+      <p className="organiser_create_event_sub-title_disclaimer">Inputs marked with * are required</p>
+      <Formik
+        initialValues={initialValues}
+        validate={validate}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+
+          <div className="organiser_create_event_row_div">
+            <label htmlFor="title">Title*</label>
+            <Field className="organiser_create_event-from_input" type="text" name="title" placeholder="Title" />
+            </div>
+            <ErrorMessage className="error" name="title" component="div" />
+            
+            <div className="organiser_create_event_row_div">
+              <label htmlFor="description">Description*</label>
+              <textarea className="organiser_create_event-from_input_textbox" type="text" maxLength="255" name="description"  placeholder="Description"/>
+            </div>
+            <ErrorMessage className="error" name="description" component="div" />
+            <div className="organiser_create_event_row_div">
+            <label htmlFor="picture">Picture</label>
+            <Field className="organiser_create_event-from_input" type="file" name="picture" />
+            <ErrorMessage className="error" name="file" component="div" />
+            </div>
+            <br/>
+            <div className="organiser_create_event_address-form">
+              <label htmlFor="address">Address*</label>
+              <div className="organiser_create_event_address-form_top">
+                <div className="organiser_create_event_row_div_address">
+                <Field className="organiser_create_event-from_input" type="text" name="addressStreet" placeholder="Street" />
+                <ErrorMessage className="error" name="addressStreet" component="div" />
+                <Field className="organiser_create_event-from_input" type="text" name="addressNumber"  placeholder="Number"/>
+                <ErrorMessage className="error" name="addressNumber" component="div" />
+                </div>
+              
+              <textarea className="organiser_create_event-from_input_textbox" type="text" name="addressDescription"  maxLength="255" placeholder="Describe how to get there" />
+              <ErrorMessage className="error" name="addressDescription" component="div" />
+              </div>
+            </div>
+            <br/>
+
+            <div className="organiser_create_event_row_div">
+            <label htmlFor="district">District*</label>
+            <Field as="select" className="organiser_create_event-from_input_dropdown" type="text" name="district"  placeholder="District">
+              <option value="place_1">Chełm</option>
+              <option value="place_2">Zaspa</option>
+              <option value="place_3">Śródmieście</option>
+            </Field>
+            </div>
+            <ErrorMessage className="error" name="district" component="div" />
+            <br/>
+
+            <div className="organiser_create_event_row_div">
+            <label htmlFor="categories">Categories*</label>
+            <Field as="select" className="organiser_create_event-from_input_dropdown" type="text" name="categories"  placeholder="Category">
+              <option value="help_1">Pomoc Bezdomnym</option>
+              <option value="help_2">Wyprowadzanie</option>
+            </Field>
+            <ErrorMessage className="error" name="category" component="div" />
+            </div>
+            <br/>
+            
+            <div className="checkbox_organiser_create_event-group">
+              <label htmlFor="peselVerification">
+                <Field type="checkbox" name="peselVerification" />
+                Pesel verification needed
+              </label>
+
+              <label htmlFor="volunteerAgreement">
+                <Field type="checkbox" name="volunteerAgreement" />
+                Volunteer agreement needed
+              </label>
+            </div>
+            
+            <br/>
+            
+            <p className="organiser_create_event_sub-title">Shifts</p>
+            <div className="organiser_create_event_shifts">
+              <div className="organiser_create_event_shifts_column">
+                <span>31.02.2024</span>
+                <p><span>07:00-14:00</span></p>
+                <span>ul. Wrót Baldura 666</span>
+              </div>
+              <div>
+                <span><b>Volunteers needed</b>: 4</span> 
+                <p><span><b>Minimum age required</b>: 2</span></p>
+                <span><b>Leader required</b>: No</span>
+              </div>
+              <div className="organiser_create_event_shifts_manage">
+                <button className="organiser_create_event-form_button">Edit</button>
+                <button className="organiser_create_event-form_button">Delete</button>
+              </div>
+            </div>
+
+            <div className="organiser_create_event_shifts">
+              <div className="organiser_create_event_shifts_column">
+                <span>31.02.2024</span>
+                <p><span>07:00-14:00</span></p>
+                <span>ul. Wrót Baldura 666</span>
+              </div>
+              <div>
+                <span><b>Volunteers needed</b>: 4</span>
+                <p><span><b>Minimum age required</b>: 2</span></p>
+                <span><b>Leader required</b>: No</span>
+              </div>
+              <div className="organiser_create_event_shifts_manage">
+                <button className="organiser_create_event-form_button">Edit</button>
+                <button className="organiser_create_event-form_button">Delete</button>
+              </div>
+            </div>
+            
+            <button className="organiser_create_event_shifts_button">Add shift</button>
+
+            <div className="button-group">
+            <button className="organiser_create_event-form_button" type="submit" disabled={isSubmitting}>
+              Create
+            </button>
+
+          </div>
+          </Form>
+
+        )}
+      </Formik>
+    </div>
+  );
+}
+
+export default OrganiserCreateEvent;
