7a997616952884f5714f165b192d1c4c78ef122c
Filiposki54 <filip.kierczynski@gmail.com>
2023-12-17 20:26:55 +0100

    Removed test
    Removed tests since they have to be redone

--- src/Components/MoreEventsFromOrganizer/MoreEvents.test.js
+++ src/Components/MoreEventsFromOrganizer/MoreEvents.test.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import MoreEvents from './MoreEvents';
 
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(initReactI18next) 
+  .init({
+  });
+
 describe('MoreEvents', () => {
   it('rendering without errors', () => {
     render(<MoreEvents />);
