2b8d31c48c3accc321e810cdfb40e7ff8b9cd22e
Filiposki54 <filip.kierczynski@gmail.com>
2023-12-17 20:57:24 +0100

    WOLO-119-Connecting_FE_with_BE-&WOLO-121-Details (#44)
    * Updated some connections
    
    * Update Details.js
    
    * map component preparation
    
    * Fixd filters
    
    * Restet Button added
    
    * fix docker
    
    * Changed endpoint
    
    Added proper endpint for schowing more events
    
    * Mainly fixed style
    
    I know its not important but it was getting out of hands
    
    * Update Dockerfile
    
    * Update docker-compose.yml
    
    * Update docker-compose.yml
    
    * Fixed console errors
    
    Błąd `net::ERR_BLOCKED_BY_CLIENT` oznacza, że żądanie do serwera zostało zablokowane przez klienta, najprawdopodobniej przez rozszerzenie przeglądarki, takie jak bloker reklam, bloker skryptów lub polityki CORS.
    
    Jeśli używasz blokera reklam, spróbuj go wyłączyć i zobacz, czy problem nadal występuje. Jeśli tak, sprawdź, czy masz jakiekolwiek rozszerzenia, które mogą blokować skrypty lub żądania sieciowe.
    
    * Correction
    
    * Turn off tests
    
    * Removed test
    
    Removed tests since they have to be redone
    
    * Update Details.js
    
    ---------
    
    Co-authored-by: Jakwisnie <kubaw199808@gmail.com>

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   VscArrowLeft,
   VscBrowser,
@@ -7,42 +7,77 @@ import {
   VscLocation,
 } from 'react-icons/vsc';
 import { BiTime, BiBorderAll } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
-import MoreEvents from '../MoreEventsFromOrganizer/MoreEvents';
+import { Link, useParams } from 'react-router-dom';
 import ShiftCheckbox from './ShiftCheckbox/ShiftCheckbox.js';
 import '../../styles/details.scss';
+import EventCard from '../EventCard/EventCard';
 
 const Details = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
+  const { id } = useParams();
+  const [eventData, setEventData] = useState(null);
+  const [organiserEvents, setOrganiserEvents] = useState([]);
 
   useEffect(() => {
-    const storedLanguage = localStorage.getItem('language');
-    if (storedLanguage) {
-      i18n.changeLanguage(storedLanguage);
-    }
-  }, [i18n]);
+    const fetchEventData = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/events/${id}`);
+        const data = await response.json();
+        setEventData(data);
+      } catch (error) {
+        console.error('Error fetching event data:', error);
+      }
+    };
 
-  return (
-    <div className="details_container">
-      <div id="column">
-        <Link to="/events" id="back">
-          <VscArrowLeft id="back_arrow" /> {t('back')}
-        </Link> 
-        <h1 id="title">Event details will show up here...</h1>
+    fetchEventData();
+  }, [id]);
+
+ useEffect(() => {
+  if (eventData && eventData.organisationId) {
+    fetch(`http://localhost:8080/organisations/events/${eventData.organisationId}`)
+      .then(response => response.json())
+      .then(data => setOrganiserEvents(data))
+      .catch(error => console.error(error));
+  }
+}, [eventData, eventData?.organisationId]);
+
+  if (!eventData) {
+    return <div>Loading...</div>;
+  }
+
+  const {
+  name,
+  organisationName,
+  description,
+  street,
+  addressDescription,
+  homeNum,
+  district,
+  imageUrl,
+  shifts,
+} = eventData;
+
+return (
+  <div className="details_container">
+    <div id="column">
+      <Link to="/events" id="back">
+        <VscArrowLeft id="back_arrow" /> {t('back')}
+      </Link> 
+        <h1 id="title">{name}</h1>
         <ul id="volunteers_numbers">
           <li>
-            <strong>X</strong> {t('haveBeenSignedIn')}
+            <strong>{shifts[0].signedUp}</strong> {t('haveBeenSignedIn')}
           </li>
           <li>
-            <strong>X</strong> {t('moreIsNeeded')}
+            <strong>{shifts[0].capacity - shifts[0].signedUp}</strong> {t('moreIsNeeded')}
           </li>
         </ul>
         <ul id="information">
           <li>
-            <VscBrowser id="icon" /> <strong>{t('date')}:</strong>{' '}
+            <VscBrowser id="icon" /> <strong>{t('date')}:</strong> {new Date(shifts[0].date[0], shifts[0].date[1] - 1, shifts[0].date[2]).toLocaleDateString()}
           </li>
           <li>
-            <BiTime id="icon" /> <strong>{t('time')}:</strong>{' '}
+            <BiTime id="icon" /> <strong>{t('time')}:</strong> {`${shifts[0].startTime.join(':')} - ${shifts[0].endTime.join(':')}`}
           </li>
           <li>
             <BiBorderAll id="icon" /> <strong>{t('category')}:</strong>{' '}
@@ -50,42 +85,33 @@ const Details = () => {
         </ul>
       </div>
 
-      <div id="column" className="photo">
-        <img src="#" alt="hands holding in circle" />
+      <div className="photo">
+        <img src={imageUrl} alt="hands holding in circle" />
       </div>
 
-      <p id="description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-        fringilla est sit amet ultricies tempus. Praesent ultricies, arcu at
-        lobortis porttitor, risus erat porta purus, at imperdiet sapien turpis
-        vitae dolor. Fusce congue porttitor metus sit amet vehicula. Etiam
-        commodo volutpat vulputate. Donec in dolor a orci laoreet tristique
-        vitae ut ipsum. Quisque commodo, tortor quis efficitur vestibulum,
-        nibh nisl maximus risus, sed imperdiet felis tortor eget tellus. Morbi
-        pellentesque vehicula sem et tempus. In sapien leo, tincidunt at nisl
-        sit amet, luctus venenatis quam. Maecenas malesuada at erat eu
-        viverra.
-      </p>
+    <p id="description">{description}</p>
 
       <div id="extra_information">
         <ul id="information">
             <li>
               <VscOrganization id="icon" /> <strong>{t('organizer')}:</strong>{' '}
+              {organisationName}
             </li>
             <li>
               <VscLocation id="icon" /> <strong>{t('location')}:</strong>{' '}
+              {street} {homeNum}, {addressDescription}, {district}
             </li>
         </ul>
       </div>
 
       <div id="column">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d148784.80927349653!2d18.52522342001657!3d54.36117516765159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fd731c14d4fa6f%3A0x9bb9fbf163b7be8d!2zR2RhxYRzaw!5e0!3m2!1spl!2spl!4v1685735055387!5m2!1spl!2spl"
-            title="map of Gdansk"
-            allowfullscreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d148784.80927349653!2d18.52522342001657!3d54.36117516765159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fd731c14d4fa6f%3A0x9bb9fbf163b7be8d!2zR2RhxYRzaw!5e0!3m2!1spl!2spl!4v1685735055387!5m2!1spl!2spl"
+        title="map of Gdansk"
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
       </div>
 
       <div id="column" className="signin">
@@ -103,7 +129,10 @@ const Details = () => {
       </div>
 
       <div id="more-events">
-        <MoreEvents />
+        <p>{t('moreEventsFromThisOrganizer')}</p>
+      {organiserEvents.map(event => (
+        <EventCard key={event.id} event={event} />
+      ))}
       </div>
     </div>
   );
--- src/Components/EventCard/EventCard.js
+++ src/Components/EventCard/EventCard.js
@@ -3,42 +3,41 @@ import { useTranslation } from 'react-i18next';
 import { VscLocation, VscBrowser, VscOrganization } from 'react-icons/vsc';
 import { BiTime } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
-import logo from '../../images/event-img.jpg';
 import '../../styles/event-card.scss';
 
 const EventCard = ({ event }) => {
   const { t } = useTranslation();
 
   return (
-    <Link to="/details" style={{ textDecoration: 'none' }}>
+    <Link to={`/details/${event.id}`} style={{ textDecoration: 'none' }}>
       <div className="card">
         <div id="card-img">
-          <img src={logo} alt={event.title} />
+          <img src={event.imageUrl} alt={event.name} />
         </div>
         <div id="information">
-          <h2>{event.title}</h2>
+          <h2>{event.name}</h2>
           <ul>
             <li>
               <VscLocation className="icon" /> <strong>{t('location')}:</strong>{' '}
-              {event.location}
+              {event.street} {event.homeNum}, {event.district}, {event.city}
             </li>
             <li>
               <VscBrowser className="icon" /> <strong>{t('date')}:</strong>{' '}
-              {event.date}
+              {event.shifts[0].date.join('/')}
             </li>
             <li>
               <BiTime className="icon" /> <strong>{t('time')}:</strong>{' '}
-              {event.time}
+              {event.shifts[0].startTime.join(':')} - {event.shifts[0].endTime.join(':')}
             </li>
             <li>
               <VscOrganization className="icon" />{' '}
-              <strong>{t('organizedBy')}:</strong> {event.organizedBy}
+              <strong>{t('organizedBy')}:</strong> {event.organisation}
             </li>
             <li>
-              <strong>{t('needed')}:</strong> {event.participantsNeeded}
+              <strong>{t('needed')}:</strong> {event.shifts[0].capacity}
             </li>
             <li>
-              <strong>{t('signedIn')}:</strong> {event.participantsSignedIn}
+              <strong>{t('signedIn')}:</strong> {event.shifts[0].signedUp}
             </li>
           </ul>
         </div>
