10245a9d07164f3a4806f03a68049f42183f8ad1
Jakwisnie <kubaw199808@gmail.com>
2023-12-28 18:28:44 +0100

    first version of admin user page (working with special branch from backend)

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import OrganiserCreateEvent from './Components/OrganiserCreateEvent/OrganiserCre
 import AdminHomePage from './Components/AdminHomePage/AdminHomePage.js';
 import VolunteerHomePage from './Components/VolunteerHomePage/VolunteerHomePage.js';
 import { FiltersProvider } from './Components/Filters/FiltersContext';
+import AdminUsersPage from "./Components/AdminHomePage/AdminUsersPage/AdminUsersPage";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
             <Route path="/organiserHomePage" element={<OrganiserHomePage />} />
             <Route path="/adminHomePage" element={<AdminHomePage />} />
             <Route path="/volunteerHomePage" element={<VolunteerHomePage />} />
+            <Route path="/adminUsersPage" element={<AdminUsersPage />} />
             <Route path="/createEvent" element={<OrganiserCreateEvent />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
