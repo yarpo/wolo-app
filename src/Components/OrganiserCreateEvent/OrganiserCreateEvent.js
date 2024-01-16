0474e9635b06a74d8b105b65d11ab1b7eded0619
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-01-16 14:06:05 +0100

    WOLO-132-translations eng pl added, smaller font for sub title
    WOLO-132-translations eng pl added, smaller font for sub title

--- src/Components/OrganiserCreateEvent/OrganiserCreateEvent.js
+++ src/Components/OrganiserCreateEvent/OrganiserCreateEvent.js
@@ -1,7 +1,17 @@
 import { Formik, Field, Form, ErrorMessage } from "formik";
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
 import '../../styles/organiser-create-event.scss';
 
 const OrganiserCreateEvent = () => {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    const storedLanguage = localStorage.getItem('language');
+    if (storedLanguage) {
+      i18n.changeLanguage(storedLanguage);
+    }
+  }, [i18n]);
 
   const initialValues = {
     title: '',
@@ -52,8 +62,8 @@ const OrganiserCreateEvent = () => {
 
   return (
     <div className="organiser_create_event_div">
-      <h1 className="organiser_create_event_title">Create Event</h1>
-      <p className="organiser_create_event_sub-title_disclaimer">Inputs marked with * are required</p>
+      <h1 className="organiser_create_event_title">{t('createEvent')}</h1>
+      <p className="organiser_create_event_sub-title_disclaimer">{t('inputsAreRequired')}</p>
       <Formik
         initialValues={initialValues}
         validate={validate}
@@ -66,24 +76,24 @@ const OrganiserCreateEvent = () => {
           <Form>
 
           <div className="organiser_create_event_row_div">
-            <label htmlFor="title">Title*</label>
+            <label htmlFor="title">{t('title')}*</label>
             <Field className="organiser_create_event-from_input" type="text" name="title" placeholder="Title" />
             </div>
             <ErrorMessage className="error" name="title" component="div" />
             
             <div className="organiser_create_event_row_div">
-              <label htmlFor="description">Description*</label>
+              <label htmlFor="description">{t('description')}*</label>
               <textarea className="organiser_create_event-from_input_textbox" type="text" maxLength="255" name="description"  placeholder="Description"/>
             </div>
             <ErrorMessage className="error" name="description" component="div" />
             <div className="organiser_create_event_row_div">
-            <label htmlFor="picture">Picture</label>
+            <label htmlFor="picture">{t('imageURL')}</label>
             <Field className="organiser_create_event-from_input" type="file" name="picture" />
             <ErrorMessage className="error" name="file" component="div" />
             </div>
             <br/>
             <div className="organiser_create_event_address-form">
-              <label htmlFor="address">Address*</label>
+              <label htmlFor="address">{t('address')}*</label>
               <div className="organiser_create_event_address-form_top">
                 <div className="organiser_create_event_row_div_address">
                 <Field className="organiser_create_event-from_input" type="text" name="addressStreet" placeholder="Street" />
@@ -99,7 +109,7 @@ const OrganiserCreateEvent = () => {
             <br/>
 
             <div className="organiser_create_event_row_div">
-            <label htmlFor="district">District*</label>
+            <label htmlFor="district">{t('district')}*</label>
             <Field as="select" className="organiser_create_event-from_input_dropdown" type="text" name="district"  placeholder="District">
               <option value="place_1">Chełm</option>
               <option value="place_2">Zaspa</option>
@@ -110,7 +120,7 @@ const OrganiserCreateEvent = () => {
             <br/>
 
             <div className="organiser_create_event_row_div">
-            <label htmlFor="categories">Categories*</label>
+            <label htmlFor="categories">{t('categories')}*</label>
             <Field as="select" className="organiser_create_event-from_input_dropdown" type="text" name="categories"  placeholder="Category">
               <option value="help_1">Pomoc Bezdomnym</option>
               <option value="help_2">Wyprowadzanie</option>
@@ -122,18 +132,18 @@ const OrganiserCreateEvent = () => {
             <div className="checkbox_organiser_create_event-group">
               <label htmlFor="peselVerification">
                 <Field type="checkbox" name="peselVerification" />
-                Pesel verification needed
+                {t('peselVerificationNeeded')}
               </label>
 
               <label htmlFor="volunteerAgreement">
                 <Field type="checkbox" name="volunteerAgreement" />
-                Volunteer agreement needed
+                {t('volunteerAgreementNeeded')}
               </label>
             </div>
             
             <br/>
             
-            <p className="organiser_create_event_sub-title">Shifts</p>
+            <p className="organiser_create_event_sub-title">{t('shifts')}</p>
             <div className="organiser_create_event_shifts">
               <div className="organiser_create_event_shifts_column">
                 <span>31.02.2024</span>
@@ -141,38 +151,21 @@ const OrganiserCreateEvent = () => {
                 <span>ul. Wrót Baldura 666</span>
               </div>
               <div>
-                <span><b>Volunteers needed</b>: 4</span> 
-                <p><span><b>Minimum age required</b>: 2</span></p>
-                <span><b>Leader required</b>: No</span>
+                <span><b>{t('volunteersNeeded')}</b>: 4</span> 
+                <p><span><b>{t('minimumAgeRequired')}</b>: 2</span></p>
+                <span><b>{t('leaderRequired')}</b>: No</span>
               </div>
               <div className="organiser_create_event_shifts_manage">
-                <button className="organiser_create_event-form_button">Edit</button>
-                <button className="organiser_create_event-form_button">Delete</button>
-              </div>
-            </div>
-
-            <div className="organiser_create_event_shifts">
-              <div className="organiser_create_event_shifts_column">
-                <span>31.02.2024</span>
-                <p><span>07:00-14:00</span></p>
-                <span>ul. Wrót Baldura 666</span>
-              </div>
-              <div>
-                <span><b>Volunteers needed</b>: 4</span>
-                <p><span><b>Minimum age required</b>: 2</span></p>
-                <span><b>Leader required</b>: No</span>
-              </div>
-              <div className="organiser_create_event_shifts_manage">
-                <button className="organiser_create_event-form_button">Edit</button>
-                <button className="organiser_create_event-form_button">Delete</button>
+                <button className="organiser_create_event-form_button">{t('edit')}</button>
+                <button className="organiser_create_event-form_button">{t('delete')}</button>
               </div>
             </div>
             
-            <button className="organiser_create_event_shifts_button">Add shift</button>
+            <button className="organiser_create_event_shifts_button">{t('addShift')}</button>
 
             <div className="button-group">
             <button className="organiser_create_event-form_button" type="submit" disabled={isSubmitting}>
-              Create
+            {t('createEvent')}
             </button>
 
           </div>
