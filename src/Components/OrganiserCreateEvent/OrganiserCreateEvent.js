7f62883d991bc3f2f843bfe9be8da80f89db2466
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-01-16 12:19:20 +0100

    WOLO-142-fixed json - sending works <3
    WOLO-142-fixed json - sending works <3

--- src/Components/OrganiserCreateEvent/OrganiserCreateEvent.js
+++ src/Components/OrganiserCreateEvent/OrganiserCreateEvent.js
@@ -4,43 +4,43 @@ import '../../styles/organiser-create-event.scss';
 const OrganiserCreateEvent = () => {
 
   const initialValues = {
-    title: '',
+    name: '',
     description: '',
-    picture: '',
-    addressStreet: '',
-    addressNumber: '',
+    imageUrl: '',
+    street: '',
+    homeNum: '',
     addressDescription: '',
-    district: '',
+    districtId: '',
     categories: '',
-    peselVerification: false,
+    peselVerificationRequired: false,
     volunteerAgreement: false,
   };
 
   const validate = values => {
     const errors = {};
 
-    if (!values.title) {
-      errors.title = 'Title is required';
+    if (!values.name) {
+      errors.name = 'Title is required';
     }
 
     if (!values.description) {
      errors.description = 'Description is required';
     }
 
-    if (!values.district) {
-      errors.district = 'District is required';
+    if (!values.districtId) {
+      errors.districtId = 'District is required';
     } 
 
     if (!values.categories) {
       errors.categories = 'At least one category is required';
     }
     
-    if (!values.addressStreet) {
-      errors.addressStreet = 'Address street is required';
+    if (!values.street) {
+      errors.street = 'Address street is required';
     }
 
-    if (!values.addressNumber) {
-      errors.addressNumber = 'Address number is required';
+    if (!values.homeNum) {
+      errors.homeNum = 'Address number is required';
     }
 
     return errors;
@@ -58,17 +58,17 @@ const OrganiserCreateEvent = () => {
         onSubmit={(values, { setSubmitting }) => {
           console.log(values);
 
-          values.organisation = 1;
+          values.organisationId = 1;
           values.categories = [parseInt(values.categories, 10)];
           values.shifts = [
             {
-              start_date: [9, 0],
-              end_date: [12, 30],
+              startTime: [10, 30],
+              endTime: [16, 45],
+              date: [2023, 6, 10],
+              capacity: 5,
+              isLeaderRequired: true,
+              requiredMinAge: 18,
             },
-            {
-              start_date: [10, 30], 
-              end_date: [15, 45], 
-            }
           ];
 
           const jsonData = JSON.stringify(values);
@@ -93,8 +93,8 @@ const OrganiserCreateEvent = () => {
       >
         <Form>
           <div className="organiser_create_event_row_div">
-            <label htmlFor="title">Title*</label>
-            <Field className="organiser_create_event-from_input" type="text" name="title" placeholder="Title" />
+            <label htmlFor="name">Title*</label>
+            <Field className="organiser_create_event-from_input" type="text" name="name" placeholder="Title" />
           </div>
           <ErrorMessage className="error" name="title" component="div" />
           <div className="organiser_create_event_row_div">
@@ -103,8 +103,8 @@ const OrganiserCreateEvent = () => {
           </div>
           <ErrorMessage className="error" name="description" component="div" />
           <div className="organiser_create_event_row_div">
-            <label htmlFor="picture">Picture</label>
-            <Field className="organiser_create_event-from_input" type="text" name="picture" />
+            <label htmlFor="imageUrl">Picture</label>
+            <Field className="organiser_create_event-from_input" type="text" name="imageUrl" placeholder="Picture Url" />
             <ErrorMessage className="error" name="file" component="div" />
           </div>
           <br/>
@@ -112,10 +112,10 @@ const OrganiserCreateEvent = () => {
             <label htmlFor="address">Address*</label>
             <div className="organiser_create_event_address-form_top">
               <div className="organiser_create_event_row_div_address">
-                <Field className="organiser_create_event-from_input" type="text" name="addressStreet" placeholder="Street" />
-                <ErrorMessage className="error" name="addressStreet" component="div" />
-                <Field className="organiser_create_event-from_input" type="text" name="addressNumber"  placeholder="Number"/>
-                <ErrorMessage className="error" name="addressNumber" component="div" />
+                <Field className="organiser_create_event-from_input" type="text" name="street" placeholder="Street" />
+                <ErrorMessage className="error" name="street" component="div" />
+                <Field className="organiser_create_event-from_input" type="text" name="homeNum"  placeholder="Number"/>
+                <ErrorMessage className="error" name="homeNum" component="div" />
               </div>      
               <Field as="textarea" className="organiser_create_event-from_input_textbox" type="text" maxLength="255" name="addressDescription"  placeholder="Describe how to get there"/>              
               <ErrorMessage className="error" name="addressDescription" component="div" />
@@ -123,8 +123,8 @@ const OrganiserCreateEvent = () => {
           </div>
           <br/>
           <div className="organiser_create_event_row_div">
-            <label htmlFor="district">District*</label>
-            <Field as="select" className="organiser_create_event-from_input_dropdown" type="text" name="district"  placeholder="District">
+            <label htmlFor="districtId">District*</label>
+            <Field as="select" className="organiser_create_event-from_input_dropdown" type="text" name="districtId"  placeholder="District">
               <option value="1">Centrum, Warszawa</option>
               <option value="2">Wrzeszcz, Gdańsk</option>
               <option value="3">Śródmieście, Gdańsk</option>
@@ -142,12 +142,12 @@ const OrganiserCreateEvent = () => {
           </div>
           <br/>  
           <div className="checkbox_organiser_create_event-group">
-            <label htmlFor="peselVerification">
-              <Field type="checkbox" name="peselVerification"/>
+            <label htmlFor="peselVerificationRequired">
+              <Field type="checkbox" name="peselVerificationRequired"/>
               Pesel verification needed
             </label>
-            <label htmlFor="volunteerAgreement">
-              <Field type="checkbox" name="volunteerAgreement"/>
+            <label htmlFor="agreementNeeded">
+              <Field type="checkbox" name="agreementNeeded"/>
               Volunteer agreement needed
             </label>
           </div>
