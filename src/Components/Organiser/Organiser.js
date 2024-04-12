de0f5620ec24ffee6c6c4d7be7f7cc5e32903ded
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-04-12 16:00:36 +0200

    WOLO-218-redirect when bad id
    WOLO-218-redirect when bad id

--- src/Components/Organiser/Organiser.js
+++ src/Components/Organiser/Organiser.js
@@ -7,7 +7,7 @@ import {
   VscLocation,
 } from 'react-icons/vsc';
 import { AiOutlineInstagram, AiOutlineUpload, AiFillHeart, AiOutlineFacebook, AiOutlineYoutube } from "react-icons/ai";
-import { Link, useLocation } from 'react-router-dom';
+import { useNavigate, Link, useLocation } from 'react-router-dom';
 import '../../styles/organizer.scss';
 import fetchData from '../../Utils/fetchData.js';
 import { URLS } from '../../config.js';
@@ -15,6 +15,7 @@ import EventCard from '../EventCard/EventCard.js';
 
 
 const Organiser = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const organisationId = searchParams.get('organisationId');
@@ -23,12 +24,26 @@ const Organiser = () => {
   const [organiserEvents, setOrganiserEvents] = useState([]);
 
   useEffect(() => {
-    if (!organisationId) {
-      return;
+    const fetchOrganiserData = async () => {
+      try {
+        const url = `${URLS.ORGANISATIONS}/${organisationId}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+          // If response is not OK, redirect to /events page
+          navigate('/events');
+          return;
+        }
+        const data = await response.json();
+        setOrganiserData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (organisationId) {
+      fetchOrganiserData();
     }
-    const url = `${URLS.ORGANISATIONS}/${organisationId}`;
-    fetchData(url, setOrganiserData);
-  }, [organisationId]);
+  }, [organisationId, navigate]);
 
   useEffect(() => {
     const url = `${URLS.ORGANISATIONS}/${organisationId}/events`;
@@ -47,59 +62,71 @@ const Organiser = () => {
 
   return (
     <div className="organizer_container">
-      <div id="column">
-        <Link to="/events" id="back">
-          <VscArrowLeft id="back_arrow" /> {t('back')}
-        </Link>
-        <div className='organizer_container_logo_text'>
-          <h1 id="title">{name}</h1>
-          <img src={logoUrl} alt="logo" className='organizer_container_logo' onError={(organiserData) => organiserData.target.style.display = 'none'}/>
+      {organiserData && Object.keys(organiserData).length > 0 && (
+        <div id="column">
+          <Link to="/events" id="back">
+            <VscArrowLeft id="back_arrow" /> {t('back')}
+          </Link>
+          <div className='organizer_container_logo_text'>
+            <h1 id="title">{name}</h1>
+            <img src={logoUrl} alt="logo" className='organizer_container_logo' onError={(organiserData) => organiserData.target.style.display = 'none'}/>
+          </div>
         </div>
-      </div>
-      <p id="description">
-        {description}
-      </p>
+      )}
+      {organiserData && Object.keys(organiserData).length > 0 && (
+        <p id="description">
+          {description}
+        </p>
+      )}
 
-      <div id="extra_information">
-        <ul id="information">
-          <li>
-            <VscCallOutgoing id="icon" /> <strong>{t('phone')}: </strong>{phoneNumber}
-          </li>
-          <li>
-            <VscMail id="icon" /> <strong>{t('E-mail')}: </strong>{email}
-          </li>
-        </ul>
-      </div>
+      {organiserData && Object.keys(organiserData).length > 0 && (
+        <div id="extra_information">
+          <ul id="information">
+            <li>
+              <VscCallOutgoing id="icon" /> <strong>{t('phone')}: </strong>{phoneNumber}
+            </li>
+            <li>
+              <VscMail id="icon" /> <strong>{t('E-mail')}: </strong>{email}
+            </li>
+          </ul>
+        </div>
+      )}
 
-      <div id="column">
-        <div>
-          <VscLocation id="icon" /> <strong>{t('location')}:</strong>{street} {homeNum}
+      {organiserData && Object.keys(organiserData).length > 0 && (
+        <div id="column">
+          <div>
+            <VscLocation id="icon" /> <strong>{t('location')}:</strong>{street} {homeNum}
+          </div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d148784.80927349653!2d18.52522342001657!3d54.36117516765159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fd731c14d4fa6f%3A0x9bb9fbf163b7be8d!2zR2RhxYRzaw!5e0!3m2!1spl!2spl!4v1685735055387!5m2!1spl!2spl"
+            title="map of Gdansk"
+            allowfullscreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+          <div className='icons_under_map'>
+            <AiOutlineUpload id="icon" />
+            <AiFillHeart id="icon" />
+            <AiOutlineInstagram id="icon" />
+            <AiOutlineFacebook id="icon" />
+            <AiOutlineYoutube id="icon" />
+          </div>
         </div>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d148784.80927349653!2d18.52522342001657!3d54.36117516765159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fd731c14d4fa6f%3A0x9bb9fbf163b7be8d!2zR2RhxYRzaw!5e0!3m2!1spl!2spl!4v1685735055387!5m2!1spl!2spl"
-          title="map of Gdansk"
-          allowfullscreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-        <div className='icons_under_map'>
-          <AiOutlineUpload id="icon" />
-          <AiFillHeart id="icon" />
-          <AiOutlineInstagram id="icon" />
-          <AiOutlineFacebook id="icon" />
-          <AiOutlineYoutube id="icon" />
+      )}
+      
+      {organiserData && Object.keys(organiserData).length > 0 && (
+        <div id="organiser_more_events">
+          <h2>{t('moreEventsFromThisOrganizer')}</h2>
+          <div id="organiser_more_events_container">
+            {organiserEvents.map(event => (
+              <EventCard key={event.id} event={event} id='organiser_more_events_item' />
+            ))}
+          </div>
         </div>
-      </div>
-      <div id="organiser_more_events">
-        <h2>{t('moreEventsFromThisOrganizer')}</h2>
-        <div id="organiser_more_events_container">
-          {organiserEvents.map(event => (
-            <EventCard key={event.id} event={event} id='organiser_more_events_item' />
-          ))}
-        </div>
-      </div>
+      )}
     </div>
   );
+
 };
 
 export default Organiser;
