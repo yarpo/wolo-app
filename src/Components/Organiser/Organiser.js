ab18b01c8078ece3474a30612b506a8d0d47ccb7
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-04-12 16:03:06 +0200

    WOLO-218-space added
    WOLO-218-space added

--- src/Components/Organiser/Organiser.js
+++ src/Components/Organiser/Organiser.js
@@ -95,7 +95,7 @@ const Organiser = () => {
       {organiserData && Object.keys(organiserData).length > 0 && (
         <div id="column">
           <div>
-            <VscLocation id="icon" /> <strong>{t('location')}:</strong>{street} {homeNum}
+            <VscLocation id="icon" /><strong>{t('location')}:</strong>{" "} {street} {homeNum}
           </div>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d148784.80927349653!2d18.52522342001657!3d54.36117516765159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fd731c14d4fa6f%3A0x9bb9fbf163b7be8d!2zR2RhxYRzaw!5e0!3m2!1spl!2spl!4v1685735055387!5m2!1spl!2spl"
