42ae2518826aa2a3cd55bb399cf985055b31c69f
Filiposki54 <filip.kierczynski@gmail.com>
2024-04-14 13:15:31 +0200

    Initial

--- src/Components/AdminHomePage/AdminHomePage.js
+++ src/Components/AdminHomePage/AdminHomePage.js
@@ -1,54 +1,13 @@
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import '../../styles/admin-home-page.scss';
-import { VscEdit} from "react-icons/vsc";
+
 
 const AdminHomePage = () => {
     
-    const { t } = useTranslation();
+    // const { t } = useTranslation();
 
     return (
         <div className='admin_home_page_container'>
-            <div id="admin_home_page_background_photo">
-                <div id="admin_home_page_welcome_text">
-                    <div>
-                        <h1>{t('hello')} user. {t('welcomeToWoloApp')}</h1>
-                    </div>
-                    <div>
-                        <h2>{t('yourAccountStatus')}: administartor</h2>
-                    </div>
-                </div>
-            </div>
-            <div id='admin_home_page_content'>
-                <div className='admin_home_page_text'>
-                    <h1><VscEdit id="admin_home_page_icon"/> {t('administratorPanel')}</h1>
-                </div>
-                <div id="admin_home_page_panel">
-                    <div className="admin_home_page_panel_column">
-                        <h2>{t('eventsSetup')}</h2>
-                        <div id="admin_home_page_button">
-                            {t('categories')}
-                        </div>
-                        <div id="admin_home_page_button">
-                            {t('roles')}
-                        </div>
-                        <div id="admin_home_page_button">
-                            {t('districts')}
-                        </div>
-                    </div>
-                    <div className="admin_home_page_panel_column">
-                        <h2>{t('management')}</h2>
-                        <div id="admin_home_page_button">
-                            {t('users')}
-                        </div>
-                        <div id="admin_home_page_button">
-                            {t('organisations')}
-                        </div>
-                        <div id="admin_home_page_button">
-                            {t('events')}
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 };
