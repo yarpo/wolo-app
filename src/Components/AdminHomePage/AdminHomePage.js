8b02c52965598d0a6f4695493d80e3af7d7f6306
Filiposki54 <filip.kierczynski@gmail.com>
2024-04-14 13:19:27 +0200

    debuging

--- src/Components/AdminHomePage/AdminHomePage.js
+++ src/Components/AdminHomePage/AdminHomePage.js
@@ -2,12 +2,41 @@
 import '../../styles/admin-home-page.scss';
 
 
+import { Tabs } from "flowbite-react";
+import { HiAdjustments, HiClipboardList, HiUserCircle } from "react-icons/hi";
+import { MdDashboard } from "react-icons/md";
+
 const AdminHomePage = () => {
     
     // const { t } = useTranslation();
 
     return (
         <div className='admin_home_page_container'>
+    <Tabs aria-label="Default tabs" style="default">
+      <Tabs.Item active title="Profile" icon={HiUserCircle}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Profile tabs associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item title="Dashboard" icon={MdDashboard}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tabs associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item title="Settings" icon={HiAdjustments}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Settings tabs associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item title="Contacts" icon={HiClipboardList}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Contacts tabs associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item disabled title="Disabled">
+        Disabled content
+      </Tabs.Item>
+    </Tabs>
         </div>
     )
 };
