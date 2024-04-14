7749b98ff2d0c4418e22479c4913ebc7061a5a1f
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-04-14 20:22:55 +0200

    Users

--- src/Components/AdminHomePage/AdminHomePage.js
+++ src/Components/AdminHomePage/AdminHomePage.js
@@ -3,7 +3,8 @@ import { useTranslation } from 'react-i18next';
 import '../../styles/admin-home-page.scss';
 
 import { URLS } from '../../config';
-import  fetchData  from  '../../Utils/fetchData'
+import fetchData  from  '../../Utils/fetchData';
+import fetchDataWithAuth from '../../Utils/fetchDataWithAuth.js';
 
 import { Tabs } from "flowbite-react";
 import { Table } from "flowbite-react";
@@ -11,11 +12,14 @@ import { HiAdjustments, HiClipboardList, HiUserCircle } from "react-icons/hi";
 import { MdDashboard } from "react-icons/md";
 
 const AdminHomePage = () => {
+    const [users, setUsers] = useState([]);
     const [districts, setDistricts] = useState([]);
     const [categories, setCategories] = useState([]);
     const [organisations, setOrganisations] = useState([]);
+    const token = localStorage.getItem('token');
 
     useEffect(() => {
+        fetchDataWithAuth(URLS.USERS, setUsers, token)
         fetchData(URLS.ORGANISATIONS, setOrganisations);
         fetchData(URLS.CATEGORIES, setCategories);
         fetchData(URLS.DISTRICTS, setDistricts);
@@ -25,15 +29,47 @@ const AdminHomePage = () => {
 
     return (
     <div className='admin_home_page_container'>
+        <h1 className='admin_home_page_text'>{t('administratorPanel')}</h1>
         <Tabs aria-label="Default tabs" style="default">
             <Tabs.Item active title={t('users')} icon={HiUserCircle}>
-
+                <div className="overflow-x-auto">
+                    <Table striped>
+                        <Table.Head>
+                            <Table.HeadCell>ID</Table.HeadCell>
+                            <Table.HeadCell>User Name</Table.HeadCell>
+                            <Table.HeadCell>User Email</Table.HeadCell>
+                            <Table.HeadCell>User Phone</Table.HeadCell>
+                            <Table.HeadCell>User roles</Table.HeadCell>
+                            <Table.HeadCell>Organisation Moderator</Table.HeadCell>
+                            <Table.HeadCell>Adult</Table.HeadCell>
+                            <Table.HeadCell>Pesel Verified</Table.HeadCell>
+                            <Table.HeadCell>Agreement Signed</Table.HeadCell>
+                        </Table.Head>
+                        <Table.Body className="divide-y">
+                            {users.map((user, index) => (
+                                <Table.Row key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                        {user.id}
+                                    </Table.Cell>
+                                    <Table.Cell>{user.firstName} {user.lastName}</Table.Cell>
+                                    <Table.Cell>{user.email}</Table.Cell>
+                                    <Table.Cell>{user.phoneNumber}</Table.Cell>
+                                    <Table.Cell>{user.roles.join(', ')}</Table.Cell>
+                                    <Table.Cell>{user.organisationName ? user.organisationName : '-'}</Table.Cell>
+                                    <Table.Cell>{user.adult ? 'YES' : 'NO'}</Table.Cell>
+                                    <Table.Cell>{user.peselVerified ? 'YES' : 'NO'}</Table.Cell>
+                                    <Table.Cell>{user.agreementSigned ? 'YES' : 'NO'}</Table.Cell>
+                                </Table.Row>
+                            ))}
+                        </Table.Body>
+                    </Table>
+                </div>
             </Tabs.Item>
             <Tabs.Item title={t('organisations')} icon={MdDashboard}>
                 <div className="overflow-x-auto">
                     <Table striped>
                         <Table.Head>
-                            <Table.HeadCell>Organisation ID</Table.HeadCell>
+                            <Table.HeadCell>ID</Table.HeadCell>
                             <Table.HeadCell>Organisation Name</Table.HeadCell>
                             <Table.HeadCell>Organisation Description</Table.HeadCell>
                             <Table.HeadCell>Organisation Email</Table.HeadCell>
@@ -66,7 +102,7 @@ const AdminHomePage = () => {
                 <div className="overflow-x-auto">
                     <Table striped>
                         <Table.Head>
-                            <Table.HeadCell>Category ID</Table.HeadCell>
+                            <Table.HeadCell>ID</Table.HeadCell>
                             <Table.HeadCell>Category Name</Table.HeadCell>
                         </Table.Head>
                         <Table.Body className="divide-y">
@@ -86,7 +122,7 @@ const AdminHomePage = () => {
                 <div className="overflow-x-auto">
                     <Table striped>
                         <Table.Head>
-                            <Table.HeadCell>District ID</Table.HeadCell>
+                            <Table.HeadCell>ID</Table.HeadCell>
                             <Table.HeadCell>District Name</Table.HeadCell>
                             <Table.HeadCell>City Name</Table.HeadCell>
                         </Table.Head>
