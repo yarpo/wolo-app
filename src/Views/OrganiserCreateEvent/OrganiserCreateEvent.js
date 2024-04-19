6e52e3c2280c90f2b81885a1eabc44c15b049c56
Filiposki54 <filip.kierczynski@gmail.com>
2024-04-19 17:05:27 +0200

    Redoing it from 0

--- src/Views/OrganiserCreateEvent/OrganiserCreateEvent.js
+++ src/Views/OrganiserCreateEvent/OrganiserCreateEvent.js
@@ -1,209 +1,67 @@
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import { useTranslation } from 'react-i18next';
-import { useState, useEffect } from 'react';
-import '../../styles/organiser-create-event.scss';
-import fetchData from '../../Utils/fetchData.js';
-import * as Yup from 'yup';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { URLS } from '../../config.js';
+import { Button, Checkbox, Label,  Textarea, TextInput } from 'flowbite-react';
+import { Formik, Field, Form } from 'formik';
 
 const OrganiserCreateEvent = () => {
-  
-  const { t, i18n } = useTranslation();
-  const [categories, setCategories] = useState([]);
-  const [districts, setDistricts] = useState([]);
-
-  useEffect(() => {
-      fetchData(URLS.CATEGORIES, setCategories);
-      fetchData(URLS.DISTRICTS, setDistricts);
-  }, []);
-
-  const [initialValues] = useState({
+  const user = JSON.parse(localStorage.getItem('user'));
+  const initialValues = {
     name: '',
+    organisationId: user.organisationId,
     description: '',
+    categories: [],
     imageUrl: '',
-    street: '',
-    homeNum: '',
-    addressDescription: '',
-    districtId: '',
-    categories: '',
-    shift: '',
-    peselVerificationRequired: false,
-    volunteerAgreement: false,
-    language: i18n.language.toUpperCase(),
-  });
+    shifts: [{
+      id: null,
+      startTime: '',
+      endTime: '',
+      date: '',
+      capacity: '',
+      isLeaderRequired: false,
+      requiredMinAge: '',
+      shiftDirections: '',
+      street: '',
+      homeNum: '',
+      districtId: ''
+    }],
+    cityId: '',
+    isPeselVerificationRequired: false,
+    isAgreementNeeded: false
+  };
 
-  const validationSchema = Yup.object().shape({
-    name: Yup.string()
-      .required('Title is required'),
-    description: Yup.string()
-      .required('Description is required'),
-    districtId: Yup.string()
-      .required('District is required'),
-    categories: Yup.string()
-      .required('At least one category is required'),
-    street: Yup.string()
-      .required('Address street is required'),
-    homeNum: Yup.string()
-      .required('Address number is required'),
-  });
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    console.log(values);
+    setSubmitting(false);
+    resetForm();
+  };
 
   return (
-    <div className="organiser_create_event_div">
-      <h1 className="organiser_create_event_title">{t('createEvent')}</h1>
-      <p className="organiser_create_event_sub-title_disclaimer">{t('inputsAreRequired')}</p>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={async (values, { setSubmitting }) => {
-            console.log(values);
+    <div className="flex justify-center items-center min-h-screen pt-20" style={{ minHeight: 'calc(100vh - 60px)' }}>
+      <div className="flex max-w-md flex-col gap-4">
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+          {({ isSubmitting }) => (
+            <Form>
+              <Label htmlFor="name" value="Event Title" />
+              <Field as={TextInput} id="name" type="text" sizing="md" name="name" />
 
-            values.organisationId = 1;
-            values.categories = [parseInt(values.categories, 10)];
-            values.shifts = [
-              {
-                startTime: [9, 9],
-                endTime: [12, 12],
-                date: [2024, 11, 11],
-                capacity: 7,
-                isLeaderRequired: false,
-                requiredMinAge: 18,
-              },
-            ];
+              <Label htmlFor="description" value="Description" />
+              <Field as={Textarea} id="description" sizing="md" name="description" />
 
-            delete values.shift
+              <Label htmlFor="imageUrl" value="Image URL" />
+              <Field as={TextInput} id="imageUrl" type="text" sizing="md" name="imageUrl" />
 
-            const jsonData = JSON.stringify(values);
-            console.log(jsonData);
+              <Label htmlFor="cityId" value="City ID" />
+              <Field as={TextInput} id="cityId" type="text" sizing="md" name="cityId" />
 
-            try {
-              const response2 = await fetch('http://127.0.0.1:8080/events/add', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: jsonData,
-              });
+              <Label htmlFor="isPeselVerificationRequired" value="Is PESEL Verification Required?" />
+              <Field as={Checkbox} id="isPeselVerificationRequired" name="isPeselVerificationRequired" />
 
-            if (!response2.ok) {
-              toast.error('Error adding event');
-              return;
-            }
+              <Label htmlFor="isAgreementNeeded" value="Is Agreement Needed?" />
+              <Field as={Checkbox} id="isAgreementNeeded" name="isAgreementNeeded" />
 
-            toast.success('Event added successfully');
-
-            console.log('Response:', response2);
-            } catch (error) {
-              console.error('Error:', error);
-              toast.error('An unexpected error occurred');
-            } finally {
-              setSubmitting(false);
-            }
-          }}
-        >
-        <Form>
-          <div className="organiser_create_event_row_div">
-            <label htmlFor="name">{t('title')}*</label>
-            <Field className="organiser_create_event-from_input" type="text" name="name" placeholder="Title" />
-          </div>
-          <ErrorMessage className="error" name="name" component="div" />
-          <div className="organiser_create_event_row_div">
-            <label htmlFor="description">{t('description')}*</label>
-            <Field as="textarea" className="organiser_create_event-from_input_textbox" type="text" maxLength="255" name="description"  placeholder="Description"/>
-          </div>
-          <ErrorMessage className="error" name="description" component="div" />
-          <div className="organiser_create_event_row_div">
-            <label htmlFor="imageUrl">{t('imageURL')}</label>
-            <Field className="organiser_create_event-from_input" type="text" name="imageUrl" placeholder="Picture Url" />
-            <ErrorMessage className="error" name="file" component="div" />
-          </div>
-          <br/>
-          <div className="organiser_create_event_address-form">
-            <label htmlFor="address">{t('address')}*</label>
-            <div className="organiser_create_event_address-form_top">
-              <div className="organiser_create_event_row_div_address">
-                <div className="address_error">
-                  <Field className="organiser_create_event-from_input" type="text" name="street" placeholder="Street" />
-                  <ErrorMessage className="error" name="street" component="div" />
-                </div>
-                <div className="address_error">
-                  <Field className="organiser_create_event-from_input" type="text" name="homeNum"  placeholder="Number"/>
-                  <ErrorMessage className="error" name="homeNum" component="div" />
-                </div>
-              </div>
-              <Field as="textarea" className="organiser_create_event-from_input_textbox" type="text" maxLength="255" name="addressDescription"  placeholder="Describe how to get there"/>              
-              <ErrorMessage className="error" name="addressDescription" component="div" />
-            </div>
-          </div>
-          <br/>
-          <div className="organiser_create_event_row_div">
-            <label htmlFor="districtId">{t('district')}*</label>
-            <Field as="select" className="organiser_create_event-from_input_dropdown" type="text" name="districtId"  placeholder="District">
-              <option value="" disabled >{t('SelectDistrict')}</option>
-              {districts.map(district => (
-                <option key={district.id} value={district.id}>{district.name}</option>
-              ))}
-            </Field>
-          </div>
-          <ErrorMessage className="error" name="district" component="div" />
-          <br/>
-          <div className="organiser_create_event_row_div">
-            <label htmlFor="categories">{t('categories')}*</label>
-           <Field as="select" className="organiser_create_event-from_input_dropdown" type="text" name="categories" placeholder="Category" >
-              <option value="" disabled >{t('SelectCategory')}</option>
-              {categories.map(category => (
-                <option key={category.id} value={category.id}>{category.name}</option>
-              ))}
-            </Field>
-          </div>
-          <br/>  
-          <div className="checkbox_organiser_create_event-group">
-            <label htmlFor="peselVerificationRequired">
-              <Field type="checkbox" name="peselVerificationRequired"/>
-              {t('peselVerificationNeeded')}
-            </label>
-            <label htmlFor="agreementNeeded">
-              <Field type="checkbox" name="agreementNeeded"/>
-              {t('volunteerAgreementNeeded')}
-            </label>
-          </div>
-          <br/>  
-          {/* <p className="organiser_create_event_sub-title">Shifts</p>
-          <div className="organiser_create_event_shifts">
-            <div className="organiser_create_event_shifts_column">
-              <div>
-                <Field type="number" name="shift.year" min="2023"/>
-                <Field type="number" name="shift.month" min="1" max="12"/>
-                <Field type="number" name="shift.day" min="1" max="31"/>
-              </div>
-              <div>
-              Start:
-              <Field type="number" name="shift.starthour" min="1" max="24"/>
-              :
-              <Field type="number" name="shift.startmin" min="0" max="60"/>
-                End:
-              <Field type="number" name="shift.endhour" min="1" max="24"/>
-              :
-              <Field type="number" name="shift.endmin" min="0" max="60"/>
-              </div>
-            </div>
-            <div>
-              <span><b>{t('volunteersNeeded')}</b>: <Field type="number" name="shift.volunteersNum" min="1" max="12"/></span> 
-              <p><span><b>{t('minimumAgeRequired')}</b>: <Field type="number" name="shift.minAge" min="0" max="99"/></span></p>
-            </div>
-             <div className="organiser_create_event_shifts_manage">
-               <button className="organiser_create_event-form_button">Edit</button>
-              <button className="organiser_create_event-form_button">Delete</button>
-            </div>
-          </div> */}
-          {/* <button className="organiser_create_event_shifts_button" disabled>Add shift</button> */}
-          <br/>
-          <div className="button-group">
-            <button className="organiser_create_event-form_button" type="submit">{t('createEvent')}</button>
-          </div>
-        </Form>
-      </Formik>
+              <Button type="submit" variant="primary" disabled={isSubmitting}>Submit</Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
