69b0b6be7a282952751dc3d15b19fd9d8ead12b3
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-04-22 18:10:10 +0200

    translations added
    translations added

--- src/Views/TheyNeedYou/TheyNeedYou.js
+++ src/Views/TheyNeedYou/TheyNeedYou.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useTranslation } from 'react-i18next';
 import React, { useState, useEffect } from 'react';
 import EventCard from '../../Components/EventCard/EventCard';
 import '../../styles/all-events.scss';
@@ -10,38 +11,39 @@ import { Banner } from "flowbite-react";
 import { HiX, HiLightBulb  } from "react-icons/hi";
 
 const TheyNeedYouEvents = () => {
-  const [events, setEvents] = useState([]);
+    const { t } = useTranslation();
+    const [events, setEvents] = useState([]);
 
-  useEffect(() => {
-    fetchData(URLS.THEY_NEED_YOU, setEvents);
-}, []);
+    useEffect(() => {
+        fetchData(URLS.THEY_NEED_YOU, setEvents);
+    }, []);
 
-  return (
-    <div id="container">
-        <div className='they_need_you_banner_container'>
-            <Banner>
-                <div className="flex w-full justify-between border-b border-gray-200 bg-gray-100 p-4 dark:border-gray-600 dark:bg-gray-700" id="they_need_you_banner">
-                    <div className="mx-auto flex items-center">
-                    <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-                        <HiLightBulb  className="mr-4 h-4 w-4" id="they_need_you_icon" />
-                        <span className="[&_p]:inline">
-                            Te wydarzenia odbywają się już niedługo, a nie uzyskały pełnej liczby wolontariuszy. Być może czekają właśnie na Ciebie!&nbsp;
-                        </span>
-                    </p>
+    return (
+        <div id="container">
+            <div className='they_need_you_banner_container'>
+                <Banner>
+                    <div className="flex w-full justify-between border-b border-gray-200 bg-gray-100 p-4 dark:border-gray-600 dark:bg-gray-700" id="they_need_you_banner">
+                        <div className="mx-auto flex items-center">
+                        <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                            <HiLightBulb  className="mr-4 h-4 w-4" id="they_need_you_icon" />
+                            <span className="[&_p]:inline">
+                                {t('theyNeedYouBanner')}&nbsp;
+                            </span>
+                        </p>
+                        </div>
+                        <Banner.CollapseButton color="gray" className="border-0 bg-transparent text-gray-500 dark:text-gray-400">
+                        <HiX className="h-4 w-4" />
+                        </Banner.CollapseButton>
                     </div>
-                    <Banner.CollapseButton color="gray" className="border-0 bg-transparent text-gray-500 dark:text-gray-400">
-                    <HiX className="h-4 w-4" />
-                    </Banner.CollapseButton>
-                </div>
-            </Banner>
+                </Banner>
+            </div>
+        <div className='all_events_cards'>
+            {events.map(event => (
+            <EventCard key={event.id} event={event} />
+            ))}
         </div>
-      <div className='all_events_cards'>
-        {events.map(event => (
-          <EventCard key={event.id} event={event} />
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default TheyNeedYouEvents;
