1adc5a86e3ccf184e5e73ca92eae470ef0ddc70c
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-04-22 22:03:50 +0200

    use utils for getting data on volunteer page

--- src/Views/VolunteerHomePage/VolunteerHomePage.js
+++ src/Views/VolunteerHomePage/VolunteerHomePage.js
@@ -5,55 +5,25 @@ import 'react-datepicker/dist/react-datepicker.css';
 import '../../styles/hero.scss';
 import '../../styles/volunteer-home-page.scss';
 import formatTime from '../../Utils/formatTime.js';
-import fetchUserId from '../../Utils/fetchUserId.js';
+import fetchUser from '../../Utils/fetchUser.js';
+import fetchDataWithAuth from '../../Utils/fetchDataWithAuth.js';
 import { URLS } from '../../config.js';
 import Hero from '../Hero/Hero.js';
 
 const VolunteerHomePage = () => {
-
     const { t } = useTranslation();
     const [userEvents, setUserEvents] = useState([]);
     const [userId, setId] = useState(null);
 
     useEffect(() => {
-        const fetchUserData = async () => {
-            const userId = await fetchUserId();
-            setId(userId);
-        };
-
-        fetchUserData();
-    }, []);
-
-    useEffect(() => {
-        const fetchUserEvents = async () => {
-            if (userId) {
-                try {
-                    const url = `${URLS.USERS}/${userId}/shifts`;
-                    const token = localStorage.getItem('token');
-
-                    const response = await fetch(url, {
-                        method: 'GET',
-                        headers: {
-                            'Authorization': `Bearer ${token}`,
-                            'Content-Type': 'application/json'
-                        }
-                    });
-
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-
-                    const data = await response.json();
-                    setUserEvents(data);
-                } catch (error) {
-                    console.error('Error fetching data:', error);
-                }
+        fetchUser().then(data => {
+            if (data) {
+              setId(data.id)
             }
-        };
+        })
 
-        fetchUserEvents();
-    }, [userId]);
-
+        fetchDataWithAuth(`${URLS.USERS}/${userId}/shifts`, setUserEvents, localStorage.getItem('token'))
+    }, []);
 
     if (!userEvents.length === 0) {
         return <div>{t('loading')}...</div>;
@@ -65,6 +35,23 @@ const VolunteerHomePage = () => {
             <br />
             <div id="volunteer_home_page_your_events">
                 <h2>{t('yourEvents')} </h2>
+                <br />
+                {userEvents && userEvents.map((shift) => (
+                    <YourEventVolunteer
+                        key={shift.shiftId}
+                        shiftId={shift.shiftId}
+                        userId={userId}
+                        name={shift.eventName}
+                        date={shift.date}
+                        startTime={formatTime(shift.startTime)}
+                        endTime={formatTime(shift.endTime)}
+                        street={shift.street}
+                        homeNum={shift.homeNum}
+                        city={shift.city} />
+                ))}
+            </div>
+            <div id="volunteer_home_page_your_events">
+                <h2>{t('yourEvents')} Archived</h2>
                 <br />
                 {userEvents && userEvents.map((shift) => (
                     <YourEventVolunteer
