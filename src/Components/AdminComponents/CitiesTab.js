6e3c83dd1b57a286d4a4d8e7fa990cfb462bc098
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-04-27 09:47:13 +0200

    cities display and adding

---
+++ src/Components/AdminComponents/CitiesTab.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import '../../styles/admin-home-page.scss';
+
+import { URLS } from '../../config';
+import fetchData  from  '../../Utils/fetchData';
+import { Table } from "flowbite-react";
+
+import AddCity from './addRecordModals/AddCity.js';
+import postRequestWithJson from '../../Utils/postRequestWithJson';
+
+const CitiesTab = () => {
+    const { t } = useTranslation();
+    const [cities, setCities] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+
+    useEffect(() => {
+        fetchData(URLS.CITIES, setCities);
+    }, []);
+
+    const handleModalAccept = (data) => {
+        setOpenModal(false);
+        console.log("Accepted Data:", data);
+
+        postRequestWithJson(URLS.ADD_CITIES, localStorage.getItem('token'), data, t('addOrganisationSuccess'), t('addOrganisationFail'));
+    };
+
+    const handleModalClose = () => {
+        setOpenModal(false);
+    }
+
+    return (
+        <div className="overflow-x-auto">
+            <button className="confirm_button" onClick={() => setOpenModal(true)}> Add </button>
+            {openModal && <AddCity onAccept={handleModalAccept} onClose={handleModalClose} />}
+            <Table striped>
+                <Table.Head>
+                    <Table.HeadCell>ID</Table.HeadCell>
+                    <Table.HeadCell>City Name</Table.HeadCell>
+                    <Table.HeadCell>Districts</Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="divide-y">
+                    {cities.map((city, index) => (
+                        <Table.Row key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                {city.id}
+                            </Table.Cell>
+                            <Table.Cell>{city.name}</Table.Cell>
+                            <Table.Cell>{city.districts.join(', ')}</Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table>
+        </div>
+    )
+};
+
+export default CitiesTab;
--- src/config.js
+++ src/config.js
@@ -19,6 +19,7 @@ const URLS = {
   USERS_ON_SHIFT: `${BASE_URL}/events/users?shift`,
   EVENT_USERS_PDF: `${BASE_URL}/events/users/pdf`,
   ADD_ORGANISATION: `${BASE_URL}/organisations/add`,
+  ADD_CITIES: `${BASE_URL}/cities/add`,
   USERS_WITH_NO_ROLES: `${BASE_URL}/users/volunteers`,
   THEY_NEED_YOU: `${BASE_URL}/events/need`
 };
