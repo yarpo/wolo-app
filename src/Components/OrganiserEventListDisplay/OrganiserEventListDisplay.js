a9ac3750a37010ea34986e464f8d8bf8e0831c7d
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-05-02 20:16:18 +0200

    Popup for delete

--- src/Components/OrganiserEventListDisplay/OrganiserEventListDisplay.js
+++ src/Components/OrganiserEventListDisplay/OrganiserEventListDisplay.js
@@ -1,21 +1,42 @@
 import { useTranslation } from 'react-i18next';
+import { useState, useEffect } from 'react';
 import '../../styles/organiser-event-list-display.scss';
 import formatDate from '../../Utils/formatDate.js';
 import { Link } from 'react-router-dom';
 import { URLS } from '../../config.js';
+import Confirmation from '../Popups/Confirmation.js';
 
 const OrganiserEventListDisplay = ({ event, isArchived }) => {
     const { t } = useTranslation();
+    const [confirmDelete, setConfirmDelete] = useState(false);
+    const [userConfirmed, setUserConfirmed] = useState(false);
+
+    const handleUserConfirmation = async (confirmation) => {
+        setUserConfirmed(confirmation);
+
+    };
+
+    useEffect(() => {
+        if (userConfirmed !== false) {
+            setUserConfirmed(false);
+            handleDelete()
+        }
+    }, [userConfirmed]); 
+
+    const handleDelete = () => {
+        console.log("Delete", event.id);
+        setConfirmDelete(false);
+    };
     
     return (
         <div className='organiser_event_list_display_content'>
             <div className="column">
                 <Link to={`${URLS.DETAILS}/${event.id}`}>
-                    {event.name}
+                    {event.nameEN}
                 </Link>
             </div>
             <div className="column">
-                {formatDate(event.shifts[0].date)}
+                {formatDate(event.date)}
             </div>
             <div className="column">
                 {event.city}
@@ -26,9 +47,22 @@ const OrganiserEventListDisplay = ({ event, isArchived }) => {
             {!isArchived && <div className="column">
                 <strong>{t('edit')}</strong>
             </div>}
-            {!isArchived && <div className="column">
-                <strong>{t('delete')}</strong>
-            </div>}
+            {!isArchived && 
+                <button className="column" onClick={() => setConfirmDelete(true)}><strong>{t('delete')}</strong></button>
+            }
+            <Confirmation id="sign-off"
+                    buttonName="Delete"
+                    title={t('deleteAccount')}
+                    accept={t('confirmAccountDelete')}
+                    deny={t('discard')}
+                    onAgree={() => {
+                        handleUserConfirmation(true)
+                        setConfirmDelete(false)}}
+                    onDeny={() => 
+                        setConfirmDelete(false)}
+                    openModal={confirmDelete}
+                    setOpenModal={setConfirmDelete}
+                />
         </div>
     )
 };
