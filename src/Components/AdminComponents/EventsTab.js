781d0ed5000072e6f5558c412e632b32a519a361
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-05-26 17:38:11 +0200

    search bar for event

--- src/Components/AdminComponents/EventsTab.js
+++ src/Components/AdminComponents/EventsTab.js
@@ -1,5 +1,8 @@
+import { useTranslation } from 'react-i18next';
 import React, { useState, useEffect } from 'react';
 import { VscChevronDown, VscChevronUp } from 'react-icons/vsc';
+import { HiOutlineSearch } from "react-icons/hi";
+import { TextInput } from "flowbite-react";
 import '../../styles/admin-home-page.scss';
 
 import { URLS } from '../../config';
@@ -8,12 +11,30 @@ import fetchData from '../../Utils/fetchData';
 import { Table } from "flowbite-react";
 
 const EventsTab = () => {
+    const { t } = useTranslation();
+    const eventName = `name${localStorage.getItem('i18nextLng').toUpperCase()}`;
     const [events, setEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([]);
     const [openIndex, setOpenIndex] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
-        fetchData(URLS.EVENTS, setEvents);
+        fetchData(URLS.EVENTS, (data) => {
+            setEvents(data);
+            setFilteredEvents(data);
+        });
     }, []);
+
+    useEffect(() => {
+        if (searchQuery === '') {
+            setFilteredEvents(events);
+        } else {
+            setFilteredEvents(events.filter(event =>
+                event[eventName].toLowerCase().includes(searchQuery.toLowerCase()) ||
+                event.organisation.toLowerCase().includes(searchQuery.toLowerCase())
+            ));
+        }
+    }, [searchQuery, events]);
 
     const renderShiftDetails = (shifts) => {
         return shifts.map((shift, index) => (
@@ -38,25 +59,34 @@ const EventsTab = () => {
 
     return (
         <div className="overflow-x-auto">
+            <div className="admin-panel-search-bar">
+                <TextInput
+                    type="text"
+                    placeholder="Search events"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    icon={HiOutlineSearch}
+                />
+            </div>
             <Table striped>
                 <Table.Head>
                     <Table.HeadCell>ID</Table.HeadCell>
                     <Table.HeadCell>Name</Table.HeadCell>
                     <Table.HeadCell>Organisation</Table.HeadCell>
-                    <Table.HeadCell>Categories</Table.HeadCell>
+                    <Table.HeadCell>{t('categories')}</Table.HeadCell>
                     <Table.HeadCell>City</Table.HeadCell>
                     <Table.HeadCell>More</Table.HeadCell>
                 </Table.Head>
                 <Table.Body className="divide-y">
-                    {events.map((event, index) => (
+                    {filteredEvents.map((event, index) => (
                         <React.Fragment key={index}>
                             <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                                 <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                                     {event.id}
                                 </Table.Cell>
-                                <Table.Cell>{event.name}</Table.Cell>
+                                <Table.Cell>{event[eventName]}</Table.Cell>
                                 <Table.Cell>{event.organisation}</Table.Cell>
-                                <Table.Cell>{event.categories}</Table.Cell>
+                                <Table.Cell>{event.categories.map((category, index) => index === event.categories.length - 1 ? category : category + ", ")}</Table.Cell>
                                 <Table.Cell>{event.city}</Table.Cell>
                                 <Table.Cell>
                                     <button
