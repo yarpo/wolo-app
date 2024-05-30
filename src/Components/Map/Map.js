b3c9377541bb1cfa9e2d5686ca27de8d938fd1f5
Filiposki54 <filip.kierczynski@gmail.com>
2024-05-30 13:05:22 +0200

    Revert "Delete Map.js"
    This reverts commit cd29c39c97259018151996e989f5c29f15299c3e.

---
+++ src/Components/Map/Map.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Map = ({ address }) => {
+  
+  const googleMapsUrl = `https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=${encodeURIComponent(address)}`;
+
+  return (
+    <iframe
+      src={googleMapsUrl}
+      title="map"
+      allowfullscreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+};
+
+export default Map;
