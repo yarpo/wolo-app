b01da9114c595e7138e1aff0570b007e83a67796
Agata Dobrzyniewicz <91957042+ayakiri@users.noreply.github.com>
2024-05-30 13:23:57 +0200

    WOLO-204 search bar for event (#119)
    * search bar for event
    
    * media
    
    * removed duplicate
    
    ---------
    
    Co-authored-by: Filiposki54 <filip.kierczynski@gmail.com>

--- src/Components/AdminComponents/EventsTab.js
+++ src/Components/AdminComponents/EventsTab.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import React, { useState, useEffect } from 'react';
 import { VscChevronDown, VscChevronUp } from 'react-icons/vsc';
+import { HiOutlineSearch } from "react-icons/hi";
+import { TextInput } from "flowbite-react";
 import '../../styles/admin-home-page.scss';
 import { format } from 'date-fns';
 
@@ -13,16 +15,32 @@ import deleteRequest from '../../Utils/deleteRequest';
 
 const EventsTab = () => {
     const { t } = useTranslation();
+    const eventName = `name${localStorage.getItem('i18nextLng').toUpperCase()}`;
     const [events, setEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([]);
     const [openIndex, setOpenIndex] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
     const [confirmDelete, setConfirmDelete] = useState(false);
     const [userConfirmed, setUserConfirmed] = useState(false);
     const [eventToDelete, setEventToDelete] = useState(null);
-    const eventName = `name${localStorage.getItem('i18nextLng').toUpperCase()}`;
 
     useEffect(() => {
-        fetchData(URLS.EVENTS, setEvents);
+        fetchData(URLS.EVENTS, (data) => {
+            setEvents(data);
+            setFilteredEvents(data);
+        });
     }, []);
+
+    useEffect(() => {
+        if (searchQuery === '') {
+            setFilteredEvents(events);
+        } else {
+            setFilteredEvents(events.filter(event =>
+                event[eventName].toLowerCase().includes(searchQuery.toLowerCase()) ||
+                event.organisation.toLowerCase().includes(searchQuery.toLowerCase())
+            ));
+        }
+    }, [searchQuery, events]);
 
     const renderShiftDetails = (shifts) => {
         return shifts.map((shift, index) => (
@@ -65,18 +83,27 @@ const EventsTab = () => {
 
     return (
         <div className="overflow-x-auto">
+            <div className="admin-panel-search-bar">
+                <TextInput
+                    type="text"
+                    placeholder="Search events"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    icon={HiOutlineSearch}
+                />
+            </div>
             <Table striped>
                 <Table.Head>
                     <Table.HeadCell>ID</Table.HeadCell>
                     <Table.HeadCell>Name</Table.HeadCell>
                     <Table.HeadCell>Organisation</Table.HeadCell>
-                    <Table.HeadCell>Categories</Table.HeadCell>
+                    <Table.HeadCell>{t('categories')}</Table.HeadCell>
                     <Table.HeadCell>City</Table.HeadCell>
                     <Table.HeadCell>More</Table.HeadCell>
                     <Table.HeadCell>{t('delete')}</Table.HeadCell>
                 </Table.Head>
                 <Table.Body className="divide-y">
-                    {events.map((event, index) => (
+                    {filteredEvents.map((event, index) => (
                         <React.Fragment key={index}>
                             <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                                 <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
@@ -84,7 +111,7 @@ const EventsTab = () => {
                                 </Table.Cell>
                                 <Table.Cell>{event[eventName]}</Table.Cell>
                                 <Table.Cell>{event.organisation}</Table.Cell>
-                                <Table.Cell>{event.categories}</Table.Cell>
+                                <Table.Cell>{event.categories.map((category, index) => index === event.categories.length - 1 ? category : category + ", ")}</Table.Cell>
                                 <Table.Cell>{event.city}</Table.Cell>
                                 <Table.Cell>
                                     <button
