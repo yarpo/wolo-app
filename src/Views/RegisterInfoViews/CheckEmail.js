19df9168ea2c2886677e76980624b76cb04eeb75
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2024-06-12 12:23:30 +0200

    style, translations

--- src/Views/RegisterInfoViews/CheckEmail.js
+++ src/Views/RegisterInfoViews/CheckEmail.js
@@ -3,24 +3,24 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Card } from "flowbite-react";
-import '../../styles/reset-password.scss';
+import '../../styles/registration.scss';
 import { Link } from 'react-router-dom';
+import { HiMail } from "react-icons/hi";
 
 
 const CheckEmail = () => {
     const { t } = useTranslation();
 
     return (
-        <div className='reset-password'>
-            <div className="reset-password-container">
-                <Card className='reset-password-card'>
-                    <h2>{t('registrationInProgress')} </h2>
-                    <p className='reset-password-tip'> 
-                        {t('checkYourEmail')}
-                    </p>
-                    <div className='reset-password-buttons'>
+        <div className='registration'>
+            <div className="registration-container">
+                <Card className='registration-card'>
+                    <HiMail className='registration-svg' />
+                    <h2>{t('registrationInProgress')}</h2>
+                    <p className='registration-tip'>{t('checkYourEmail')}</p>
+                    <div className='registration-buttons'>
                         <Link to="/login" id="back">
-                            <button className='confirm_button'>{t('confirm')}</button>
+                        <button className='confirm_button'>{t('confirm')}</button>
                         </Link>
                     </div>
                 </Card>
--- src/Views/RegisterInfoViews/RegistrationSuccess.js
+++ src/Views/RegisterInfoViews/RegistrationSuccess.js
@@ -3,22 +3,24 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Card } from "flowbite-react";
-import '../../styles/reset-password.scss';
+import '../../styles/registration.scss';
 import { Link } from 'react-router-dom';
+import { HiCheck } from "react-icons/hi";
 
 
 const RegistartionSuccess = () => {
     const { t } = useTranslation();
 
     return (
-        <div className='reset-password'>
-            <div className="reset-password-container">
-                <Card className='reset-password-card'>
+        <div className='registration'>
+            <div className="registration-container">
+                <Card className='registration-card'>
+                    <HiCheck className='registration-svg' />
                     <h2>{t('registrationCompleted')} </h2>
-                    <p className='reset-password-tip'> 
+                    <p className='registration-tip'> 
                         {t('youCanNowLogIn')}
                     </p>
-                    <div className='reset-password-buttons'>
+                    <div className='registration-buttons'>
                         <Link to="/login" id="back">
                             <button className='confirm_button'>{t('login')}</button>
                         </Link>
